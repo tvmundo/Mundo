5bd4ad68babfb7967081e95e15730535299da95e
tvmundo <43994174+tvmundo@users.noreply.github.com>
2019-03-02 23:59:45 -0300

    Add files via upload

---
+++ music-player/demo/js/myplaylist.js
@@ -0,0 +1,144 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'Because i love you.mp3',
+        oga:'mix/1.ogg',
+        title:'Because i love you',
+        artist:'Eminem',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/1.jpg'
+    },
+    {
+        mp3:'Mandy.mp3',
+        oga:'mix/2.ogg',
+        title:'Mandy.mp3',
+        artist:'Katy Perry',
+        rating:3,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/2.jpg'
+
+    },
+    {
+        mp3:'Reality.mp3',
+        oga:'mix/3.ogg',
+        title:'Reality.mp3',
+        artist:'Enrique Iglesias',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/3.jpg'
+    },
+    {
+        mp3:'To Sir With Love   .mp3',
+        oga:'mix/4.ogg',
+        title:'To Sir With Love',
+        artist:'Katy Perry',
+        rating:2,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/4.jpg'
+    },
+    {
+        mp3:'You Know My Dreams.mp3',
+        oga:'mix/5.ogg',
+        title:'You Know My Dreams',
+        artist:'Mike Posner',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/5.jpg'
+    },
+    {
+        mp3:'mix/6.mp3',
+        oga:'mix/6.ogg',
+        title:'Mine',
+        artist:'Taylor Swift',
+        rating:2.5,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/6.jpg'
+    },
+    {
+        mp3:'mix/7.mp3',
+        oga:'mix/7.ogg',
+        title:'Dynamite',
+        artist:'Taio Cruz',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/7.jpg'
+    },
+    {
+        mp3:'mix/8.mp3',
+        title:'Watcha Say',
+        artist:'Jason Derulo',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/8.jpg'
+    },
+    {
+        mp3:'mix/9.mp3',
+        oga:'mix/9.ogg',
+        title:'Don\'t Let Me Fall',
+        artist:'B.o.B',
+        rating:3,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/9.jpg'
+    },
+    {
+        mp3:'mix/10.mp3',
+        oga:'mix/10.ogg',
+        title:'Club Can\'t Handle Me',
+        artist:'Flo Rida',
+        rating:3,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/10.jpg'
+    },
+    {
+        mp3:'mix/11.mp3',
+        oga:'mix/11.oga',
+        title:'Don\'t Let Me Fall',
+        artist:'B.o.B',
+        rating:3,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/11.jpg'
+    },
+    {
+        mp3:'mix/12.mp3',
+        oga:'mix/12.ogg',
+        title:'Love Em All',
+        artist:'Usher',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/12.jpg'
+    }
+
+];
---
+++ music-player/demo/js/myplaylist.orig.js
@@ -0,0 +1,144 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'mix/1.mp3',
+        oga:'mix/1.ogg',
+        title:'Love The Way You Lie',
+        artist:'Eminem',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/1.jpg'
+    },
+    {
+        mp3:'mix/2.mp3',
+        oga:'mix/2.ogg',
+        title:'California Gurls',
+        artist:'Katy Perry',
+        rating:3,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/2.jpg'
+
+    },
+    {
+        mp3:'mix/3.mp3',
+        oga:'mix/3.ogg',
+        title:'Cuando Me Enamoro',
+        artist:'Enrique Iglesias',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/3.jpg'
+    },
+    {
+        mp3:'mix/4.mp3',
+        oga:'mix/4.ogg',
+        title:'Teenage Dream',
+        artist:'Katy Perry',
+        rating:2,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/4.jpg'
+    },
+    {
+        mp3:'mix/5.mp3',
+        oga:'mix/5.ogg',
+        title:'31 Minute to Takeoff',
+        artist:'Mike Posner',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/5.jpg'
+    },
+    {
+        mp3:'mix/6.mp3',
+        oga:'mix/6.ogg',
+        title:'Mine',
+        artist:'Taylor Swift',
+        rating:2.5,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/6.jpg'
+    },
+    {
+        mp3:'mix/7.mp3',
+        oga:'mix/7.ogg',
+        title:'Dynamite',
+        artist:'Taio Cruz',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/7.jpg'
+    },
+    {
+        mp3:'mix/8.mp3',
+        title:'Watcha Say',
+        artist:'Jason Derulo',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/8.jpg'
+    },
+    {
+        mp3:'mix/9.mp3',
+        oga:'mix/9.ogg',
+        title:'Don\'t Let Me Fall',
+        artist:'B.o.B',
+        rating:3,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/9.jpg'
+    },
+    {
+        mp3:'mix/10.mp3',
+        oga:'mix/10.ogg',
+        title:'Club Can\'t Handle Me',
+        artist:'Flo Rida',
+        rating:3,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/10.jpg'
+    },
+    {
+        mp3:'mix/11.mp3',
+        oga:'mix/11.oga',
+        title:'Don\'t Let Me Fall',
+        artist:'B.o.B',
+        rating:3,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/11.jpg'
+    },
+    {
+        mp3:'mix/12.mp3',
+        oga:'mix/12.ogg',
+        title:'Love Em All',
+        artist:'Usher',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/12.jpg'
+    }
+
+];
